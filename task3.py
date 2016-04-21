b7bf686da1085ff7bce1b6a4d34c3be212006f10
Ken Price <kaprice1@asu.edu>
2016-04-20 18:51:16 -0700

    Add tasks 2 and 3

---
+++ task3.py
@@ -0,0 +1,64 @@
+# TASK 3:  Neural network with 2 hidden layers with 100 neurons in first hidden layer and 15 neurons in the second hidden layer.
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+import tensorflow as tf
+
+INPUT_SIZE = 784         # 16x16 pixel images of handwritten numbers = 784 pixels each
+CLASSIFICATION_SIZE = 10 # Numbers 0 to 9
+HID_LAYER_1 = 100
+HID_LAYER_2 = 15
+
+#################################################
+# INPUT / OUTPUT
+#################################################
+# Image flattened to a vector of 784 pixels
+x = tf.placeholder(tf.float32, [None, INPUT_SIZE])
+# Image flattened to a vector of 784 pixels
+y = tf.placeholder(tf.float32, [None, CLASSIFICATION_SIZE])
+
+
+#################################################
+#MODEL
+#################################################
+# weight and bias matrices
+w       = [tf.Variable(tf.random_normal([INPUT_SIZE,                 HID_LAYER_1])),
+           tf.Variable(tf.random_normal([HID_LAYER_1,                HID_LAYER_2]))]
+w_out   =  tf.Variable(tf.random_normal([HID_LAYER_2,                CLASSIFICATION_SIZE]))
+
+b       = [tf.Variable(tf.random_normal([HID_LAYER_1])),
+           tf.Variable(tf.random_normal([HID_LAYER_2]))]
+b_out   =  tf.Variable(tf.random_normal([CLASSIFICATION_SIZE]))
+
+# Hidden layers
+layer_1 = tf.nn.relu(tf.add(tf.matmul(x, w[0]), b[0]))
+layer_2 = tf.nn.relu(tf.add(tf.matmul(layer_1, w[1]), b[1]))
+model = tf.matmul(layer_2, w_out) + b_out
+
+# OPTIMIZATION
+# Softmax cross entropy
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(model, y))
+# Other optimization methods can be used besides Gradient Descent
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
+
+# Init and start session
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+for epoch in range(20):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples/100)
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys})
+        avg_cost += sess.run(cost, feed_dict={x: batch_xs, y: batch_ys})/total_batch
+
+    print "Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost)
+
+
+# TEST Measuring effectiveness
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(model,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
